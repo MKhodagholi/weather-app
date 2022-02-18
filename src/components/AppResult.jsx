0a1032b3d39f44d269d1f230f717bd90d711f94f
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-18 19:52:01 +0330

    Add AppResult Component

--- src/components/AppResult.jsx
+++ src/components/AppResult.jsx
@@ -4,28 +4,45 @@ import dateFormat from "dateformat";
 import classes from "./AppResult.module.css";
 
 const AppResult = ({ information }) => {
-  const date = dateFormat("2022-02-18T12:59:07.948570Z", "dddd, mmmm dS, yyyy");
+  const { time, nameCity, weather_name, state_abbr, temp, minTemp, maxTemp } =
+    information;
 
+  const cityName = nameCity[0].toUpperCase() + nameCity.slice(1);
+
+  const timeConverted = dateFormat(time, "dddd, mmmm dS, yyyy");
+  const tempInt = temp;
+  const minTempInt = minTemp;
+  const maxTempInt = maxTemp;
+  const img = `https://www.metaweather.com/static/img/weather/${state_abbr}.svg`;
   return (
     <div className={classes["app-result"]}>
       <div className={classes.icon}>
-        <img src="https://www.metaweather.com/static/img/weather/c.svg" />
+        <img src={img} />
       </div>
       <div className={classes["name-date"]}>
-        <h3 className="name">Lagos</h3>
-        <h3 className={classes.date}>{date}</h3>
+        <h3 className="name">{cityName}</h3>
+        <h3 className={classes.date}>{timeConverted}</h3>
       </div>
       <hr />
       <div className={classes["temp-part"]}>
         <h3>
-          Temp: <span className={classes.cold}>25˚C</span>
+          Temp:{" "}
+          <span className={`${minTempInt > 15 ? classes.hot : classes.cold}`}>
+            {tempInt}˚C
+          </span>
         </h3>
         <h3>
-          Weather: <span className={classes.weather}>Clouds</span>
+          Weather: <span className={classes.weather}> Clouds</span>
         </h3>
         <h3>
-          Temp Range: <span className={classes.cold}>24.6˚C</span> to
-          <span className={classes.hot}> 25.3˚C</span>
+          Temp Range:{" "}
+          <span className={`${minTempInt > 15 ? classes.hot : classes.cold}`}>
+            {minTempInt}˚C
+          </span>{" "}
+          to{" "}
+          <span className={`${minTempInt > 15 ? classes.hot : classes.cold}`}>
+            {maxTempInt}˚C
+          </span>
         </h3>
       </div>
     </div>
