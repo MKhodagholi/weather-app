1c15464dedbfb25947b9880bc380ca09f67405dd
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-18 20:40:19 +0330

    add try and catch and correct the fields

--- src/App.js
+++ src/App.js
@@ -22,28 +22,36 @@ const App = () => {
 
   const getInformation = async () => {
     setIsLoading(true);
-    const res = await fetch(
-      `https://www.metaweather.com/api/location/search/?query=${city}`
-    );
-    const woeidJson = await res.json();
-    const woeid = await woeidJson[0].woeid;
-    const resCity = await fetch(
-      `https://www.metaweather.com/api/location/${woeid}/`
-    ).json();
-    const resCityJson = await resCity.json();
-    const data = await resCityJson.consolidated_weather[0];
-    const information = {
-      nameCity: data.title,
-      time: data.created,
-      weather_name: data.weather_state_name,
-      state_abbr: data.weather_state_abbr,
-      temp: data.the_temp,
-      minTemp: data.min_temp,
-      maxTemp: data.max_temp,
-    };
-    setInformation(information);
-    setCity("");
-    setIsLoading(false);
+    try {
+      const res = await fetch(
+        `https://www.metaweather.com/api/location/search/?query=${city}`,
+        { mode: "no-cors" }
+      );
+      const woeidJson = await res.json();
+      const woeid = await woeidJson[0].woeid;
+      const resCity = await fetch(
+        `https://www.metaweather.com/api/location/${woeid}/`
+      ).json();
+      const resCityJson = await resCity.json();
+      const data = await resCityJson.consolidated_weather[0];
+      const information = {
+        nameCity: data.title,
+        time: data.created,
+        weather_name: data.weather_state_name,
+        state_abbr: data.weather_state_abbr,
+        temp: data.the_temp,
+        minTemp: data.min_temp,
+        maxTemp: data.max_temp,
+      };
+      setInformation(information);
+      setCity("");
+      setIsLoading(false);
+      setTimeout(() => {
+        setInformation(null);
+      }, 15000);
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
