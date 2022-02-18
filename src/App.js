186b72e6ff24b8b1eff3541cfe877ffbbe4a711b
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-18 19:51:40 +0330

    Seperated App into the Components

--- src/App.js
+++ src/App.js
@@ -2,66 +2,53 @@ import React, { useState } from "react";
 import dateFormat from "dateformat";
 
 import classes from "./App.module.css";
+import AppSearch from "./components/AppSearch";
+import AppResult from "./components/AppResult";
 
 const App = () => {
   const [isDark, setIsDark] = useState(false);
+  const [city, setCity] = useState("");
+  const [information, setInformation] = useState(null);
+
+  const changeCityHandler = (e) => {
+    setCity(e.target.value);
+  };
 
   const changeThemeHandler = () => {
     setIsDark((prevStateTheme) => !prevStateTheme);
   };
 
-  const getLocation = async () => {
+  const getInformation = async () => {
     const res = await fetch(
-      "https://www.metaweather.com/api/location/search/?query=san",
-      { mode: "cors" }
-    );
-    const data = res.consolidated_weather[0];
+      `https://www.metaweather.com/api/location/search/?query=${city}`
+    ).json();
+    const woeid = await res[0].woeid;
+    const resCity = await fetch(
+      `https://www.metaweather.com/api/location/${woeid}/`
+    ).json();
+    const data = await resCity.consolidated_weather[0];
     const information = {
       nameCity: data.title,
-      weather_state_name: data.weather_state_name,
-      weather_state_abbr: data.weather_state_abbr,
+      time: data.created,
+      weather_name: data.weather_state_name,
+      state_abbr: data.weather_state_abbr,
       temp: data.the_temp,
       minTemp: data.min_temp,
       maxTemp: data.max_temp,
     };
+    setInformation(information);
+    setCity("");
   };
-
-  const date = dateFormat("2022-02-18T12:59:07.948570Z", "dddd, mmmm dS, yyyy");
 
   return (
     <div className={`${isDark ? classes.dark : classes.light} ${classes.app}`}>
       <h1 className={classes.title}>Weather App</h1>
-      <div className={classes["app-search"]}>
-        <input
-          className={classes["search-input"]}
-          placeholder="Search By City Name"
-        />
-        <button onClick={getLocation} className={classes["search-button"]}>
-          Search
-        </button>
-      </div>
-      <div className={classes["app-result"]}>
-        <div className={classes.icon}>
-          <img src="https://www.metaweather.com/static/img/weather/c.svg" />
-        </div>
-        <div className={classes["name-date"]}>
-          <h3 className="name">Lagos</h3>
-          <h3 className={classes.date}>{date}</h3>
-        </div>
-        <hr />
-        <div className={classes["temp-part"]}>
-          <h3>
-            Temp: <span className={classes.cold}>25˚C</span>
-          </h3>
-          <h3>
-            Weather: <span className={classes.weather}>Clouds</span>
-          </h3>
-          <h3>
-            Temp Range: <span className={classes.cold}>24.6˚C</span> to
-            <span className={classes.hot}> 25.3˚C</span>
-          </h3>
-        </div>
-      </div>
+      <AppSearch
+        changeCityHandler={changeCityHandler}
+        getInformation={getInformation}
+        city={city}
+      />
+      {information && <AppResult information={information} />}
       <button
         className={classes["theme-switcher"]}
         onClick={changeThemeHandler}
