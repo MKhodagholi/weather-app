9b3b32334cd039a6b6a94079a4d40f9d3738a2c6
MKhodagholi <MKhodaagholi@gmail.com>
2022-02-18 17:19:54 +0330

    finish UI For Search Section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,18 @@ import React, { useState } from "react";
 import classes from "./App.module.css";
 
 const App = () => {
-  const [isDark, setIsDark] = useState(true);
+  const [isDark, setIsDark] = useState(false);
 
   return (
-    <div className={"app"`${isDark ? "dark" : "light"}`}>
-      <h1 className="title"></h1>
+    <div className={`${isDark ? classes.dark : classes.light} ${classes.app}`}>
+      <h1 className={classes.title}>Weather App</h1>
+      <div className={classes["app-search"]}>
+        <input
+          className={classes["search-input"]}
+          placeholder="Search By City Name"
+        />
+        <button className={classes["search-button"]}>Search</button>
+      </div>
     </div>
   );
 };
